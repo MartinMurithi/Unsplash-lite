bbd37c5722ca8a10fc6d9028a9d52d283df24fca
MartinMurithi <wachiramartin442@gmail.com>
2023-05-12 23:03:48 +0300

    Infinite scrolling in search data coming soon

--- src/Componenets/Photos/Images.jsx
+++ src/Componenets/Photos/Images.jsx
@@ -6,7 +6,7 @@ import { Dna } from "react-loader-spinner";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
-function Images  ()  {
+function Images() {
   const fetchImages = async (page = 1) => {
     const response = await axios.get(
       `https://api.unsplash.com/photos?client_id=W5R_xL3DvFvEfEY2PmyR3uTzhaRMT3xZv_53VS9OF4I&per_page=30&page=${page}`
@@ -28,14 +28,12 @@ function Images  ()  {
 
   useEffect(() => {
     let fetching = false;
-
     const onScroll = async (event) => {
       // ScrollHeight, the height of the entire document, including non-visible content
       // Scroll top,  the number of pixels that the document is currently scrolled vertically
       // client height, the height of the visible area of the document
       const { scrollHeight, scrollTop, clientHeight } =
         event.target.scrollingElement;
-
       if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
         fetching = true;
         if (hasNextPage) await fetchNextPage();
@@ -57,12 +55,12 @@ function Images  ()  {
     return <p>ERROR: {error.message}</p>;
   }
 
-  // console.log(data);
+  console.log(data);
   return (
     <div className="imagesList">
       <ImageList variant="masonry" cols={3} gap={13}>
         {data.pages.map((page) =>
-          page.map((image) => (
+          page.map((image) =>(
             <ImageListItem key={image.id}>
               <img
                 className="image"
@@ -71,15 +69,15 @@ function Images  ()  {
                 color={image.color}
                 loading="lazy"
               />
-                <p className="authorNames">
-                  {image.user?.first_name } {image.user?.last_name}
-                </p>
+              <p className="authorNames">
+                {image.user?.first_name} {image.user?.last_name}
+              </p>
             </ImageListItem>
           ))
         )}
       </ImageList>
     </div>
   );
-};
+}
 
 export default Images;
--- src/Pages/SearchData/SearchData.jsx
+++ src/Pages/SearchData/SearchData.jsx
@@ -1,21 +1,59 @@
-import React, { useState } from "react";
-import { useQuery } from "react-query";
+import React, { useEffect, useState } from "react";
+import { useInfiniteQuery, useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 function SearchData() {
   const [search, setSearch] = useState("");
   const SEARCH_API_URL = `https://api.unsplash.com/search/photos?client_id=W5R_xL3DvFvEfEY2PmyR3uTzhaRMT3xZv_53VS9OF4I&page=1&per_page=30&query=${search}`;
 
-  const searchImages = async () => {
+  const searchImages = async (page = 1) => {
     const response = await axios.get(SEARCH_API_URL);
     return response.data;
   };
 
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: "search",
-    queryFn: searchImages,
-  });
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    refetch,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ["data"],
+    ({ pageParam = 1 }) => searchImages(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return lastPage.length > 0 ? nextPage : undefined;
+      },
+    },
+    { enabled: false }
+  );
+
+  // refetch, fetches data manually
+  // By default, react query fetches data when a component is mounted, to prevent that, set enabled = false.
+
+  useEffect(() => {
+    let fetching = false;
+    const onScroll = async (event) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement;
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+        fetching = true;
+        if (hasNextPage) await fetchNextPage();
+        fetching = false;
+      }
+    };
+
+    document.addEventListener("scroll", onScroll);
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, [fetchNextPage, hasNextPage]);
 
   if (isLoading) {
     return <ThreeDots />;
@@ -26,27 +64,50 @@ function SearchData() {
   }
 
   const handleSearch = () => {
-    console.log(data);
+    refetch();
   };
- 
-
+  console.log(data);
   return (
-    <div className="searchContainer">
-      <input
-        type="search"
-        name="search"
-        id="searchInput"
-        placeholder="Search high resolution images"
-        value={search}
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-      />
-      <button className="searchBtn" onClick={handleSearch}>
-        Search
-      </button>
-      <p className="searchText">Trending: 3D, Nature, Wallpapers, love, film</p>
-    </div>
+    <>
+      <div>
+        <input
+          type="search"
+          id="searchInput"
+          placeholder="Search high resolution images"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        <button className="searchBtn" onClick={handleSearch}>
+          Search
+        </button>
+        <p className="searchText">
+          Trending: 3D, Nature, Wallpapers, love, film
+        </p>
+      </div>
+
+      <div className="imageList">
+        <ImageList variant="masonry" cols={3} gap={13} children>
+        {data.pages.map((page) =>
+          page.results?.map((image) =>(
+            <ImageListItem key={image.id}>
+              <img
+                className="image"
+                src={image.urls?.small}
+                alt={image.alt_description}
+                color={image.color}
+                loading="lazy"
+              />
+              <p className="authorNames">
+                {image.user?.first_name} {image.user?.last_name}
+              </p>
+            </ImageListItem>
+          ))
+        )}
+      </ImageList>
+      </div>
+    </>
   );
 }
 
